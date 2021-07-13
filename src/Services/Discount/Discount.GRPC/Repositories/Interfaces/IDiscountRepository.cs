300dbfaf07a54bf215d20b665037e177c6ef1684
Michal S Fila <michal.fila@coats.com>
2021-07-13 13:06:29 +0100

    Add Discount.GRPC project

---
+++ src/Services/Discount/Discount.GRPC/Repositories/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Discount.GRPC.Entities;
+
+namespace Discount.GRPC.Repositories.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscountAsync(string productName);
+        Task<bool> CreateDiscountAsync(Coupon coupon);
+        Task<bool> UpdateDiscountAsync(Coupon coupon);
+        Task<bool> DeleteDiscountAsync(string productName);
+    }
+}
